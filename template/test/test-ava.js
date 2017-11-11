76ace47074fd17ae124e0c979419461efd864a66
Adam Kiss <iam@adamkiss.com>
2017-11-11 19:48:21 +0100

    Fix some issues: xo stuff, wrong packages in tests, etc.

--- template/test/test-ava.js
+++ template/test/test-ava.js
@@ -1,34 +1,34 @@
-const join = require('path').join;
-const Taskr = require('taskr');
-const test = require('ava');
+const join = require('path').join
+const Taskr = require('taskr')
+const test = require('ava')
 
-const dir = join(__dirname, 'fixtures');
-const plugins = [require('taskr-clear'), require('../')];
+const dir = join(__dirname, 'fixtures')
+const plugins = [require('@taskr/clear'), require('../')]
 
-const tmpDir = str => join(__dirname, str);
-const create = tasks => new Taskr({ tasks, plugins });
+const tmpDir = str => join(__dirname, str)
+const create = tasks => new Taskr({tasks, plugins})
 
 test('attach `<%= name %>` plugin to instance', t => {
-	const taskr = create();
-	t.ok('<%= slugName %>' in taskr.plugins);
-});
+	const taskr = create()
+	t.ok('<%= slugName %>' in taskr.plugins)
+})
 
 test('attach `<%= slugName %>` to Task instance', t => {
 	create({
-		*foo(task) {
-			t.ok('<%= slugName %>' in task);
+		* foo(task) {
+			t.ok('<%= slugName %>' in task)
 		}
-	}).start('foo');
-});
+	}).start('foo')
+})
 
 test('example usage test', t => {
 	create({
-		*foo(task) {
-			const tmp = tmpDir('tmp1');
-			yield f.source(`${dir}/*.js`).target(tmp);
-			const arr = yield f.$.expand(`${tmp}/*.js`);
-			t.equal(arr.length, 1, 'copied one file to target tar');
-			yield f.clear(tmp); // cleanup
+		* foo(task) {
+			const tmp = tmpDir('tmp1')
+			yield task.source(`${dir}/*.js`).target(tmp)
+			const arr = yield task.$.expand(`${tmp}/*.js`)
+			t.equal(arr.length, 1, 'copied one file to target tar')
+			yield task.clear(tmp) // Cleanup
 		}
-	}).start('foo');
-});
+	}).start('foo')
+})
--- template/test/test-tape.js
+++ template/test/test-tape.js
@@ -1,26 +1,26 @@
-const join = require('path').join;
-const Taskr = require('taskr');
-const test = require('tape');
+const join = require('path').join
+const Taskr = require('taskr')
+const test = require('tape')
 
-const dir = join(__dirname, 'fixtures');
-const plugins = [require('taskr-clear'), require('../')];
+const dir = join(__dirname, 'fixtures')
+const plugins = [require('@taskr/clear'), require('../')]
 
-const tmpDir = str => join(__dirname, str);
-const create = tasks => new Taskr({ tasks, plugins });
+const tmpDir = str => join(__dirname, str)
+const create = tasks => new Taskr({tasks, plugins})
 
 test('<%= slugName %>', t => {
-	t.plan(3);
+	t.plan(3)
 	const taskr = create({
-		*foo(task) {
-			t.true('<%= slugName %>' in task, 'attach `<%= name %>` to Task instance');
-			t.true('<%= slugName %>' in taskr.plugins, 'attach `<%= name %>` plugin to instance');
-			// example usage test
-			const tmp = tmpDir('tmp1');
-			yield f.source(`${dir}/*.js`).target(tmp);
-			const arr = yield f.$.expand(`${tmp}/*.js`);
-			t.equal(arr.length, 1, 'copied one file to target tar');
-			yield f.clear(tmp); // cleanup
+		* foo(task) {
+			t.true('<%= slugName %>' in task, 'attach `<%= name %>` to Task instance')
+			t.true('<%= slugName %>' in taskr.plugins, 'attach `<%= name %>` plugin to instance')
+			// Example usage test
+			const tmp = tmpDir('tmp1')
+			yield task.source(`${dir}/*.js`).target(tmp)
+			const arr = yield task.$.expand(`${tmp}/*.js`)
+			t.equal(arr.length, 1, 'copied one file to target tar')
+			yield task.clear(tmp) // Cleanup
 		}
-	});
-	taskr.start('foo');
-});
+	})
+	taskr.start('foo')
+})
